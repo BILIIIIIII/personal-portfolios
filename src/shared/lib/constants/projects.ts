2e2be963f95fed91930244fd1c463002d596cc6a
BILIIIIIII <ntah29973@gmail.com>
2025-08-11 07:24:05 +0700

    feat(portfolio): add new projects to data

--- src/shared/lib/constants/projects.ts
+++ src/shared/lib/constants/projects.ts
@@ -1,8 +1,20 @@
 export const projectData = [
 	{
-		link: "https://traffic-counter-mobile-programming-5.vercel.app/",
-		name: "traffic counter",
-		img: "/img/trafficcounter.png",
-		date: { day: "16", month: " DES", year: "2023" },
+		link: "https://github.com/LAI25-SM079-Laskar-AI-Capstone",
+		name: "realestica",
+		img: "/img/realestica.png",
+		date: { day: "13", month: " JUN", year: "2025" },
+	},
+	{
+		link: "https://github.com/BILIIIIIII/featured-next-starter",
+		name: "featured-next-starter",
+		img: "/img/featured-next-starter.png",
+		date: { day: "21", month: "JUL", year: "2025" },
+	},
+	{
+		link: "https://github.com/BILIIIIIII/featured-react-vite-intent",
+		name: "featured-react-vite",
+		img: "/img/featured-react-vite.png",
+		date: { day: "28", month: " JUL", year: "2025" },
 	},
 ];
